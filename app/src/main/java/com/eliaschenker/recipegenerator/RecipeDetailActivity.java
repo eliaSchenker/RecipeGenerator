8ec2b26e5f5464334b78c903a22d5728621ceca5
EliaSchenker <schenkere@bzz.ch>
2022-07-07 20:44:36 +0200

    Update RecipeDetailActivity.java
    Integrated the favorites service into the RecipeDetailActivity

--- app/src/main/java/com/eliaschenker/recipegenerator/RecipeDetailActivity.java
+++ app/src/main/java/com/eliaschenker/recipegenerator/RecipeDetailActivity.java
@@ -2,14 +2,21 @@ package com.eliaschenker.recipegenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.eliaschenker.recipegenerator.model.Ingredient;
 import com.eliaschenker.recipegenerator.model.Recipe;
+import com.eliaschenker.recipegenerator.service.FavoritesService;
+import com.eliaschenker.recipegenerator.service.RecipeAPIService;
 import com.eliaschenker.recipegenerator.service.SetImageToURLTask;
 
 public class RecipeDetailActivity extends AppCompatActivity {
@@ -24,6 +31,9 @@ public class RecipeDetailActivity extends AppCompatActivity {
     public TextView recipeIngredients;
     public TextView instructionsText;
     public ImageView recipeDetailImage;
+
+    //Favorite service
+    public FavoritesService favoritesService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +55,7 @@ public class RecipeDetailActivity extends AppCompatActivity {
         recipeDetailImage = findViewById(R.id.recipeDetailImage);
 
         initUI();
+        initFavoritesService();
     }
 
     /**
@@ -57,16 +68,58 @@ public class RecipeDetailActivity extends AppCompatActivity {
                 recipe.getArea()));
         StringBuilder ingredientsText = new StringBuilder();
         for (Ingredient ingredient : recipe.getIngredients()) {
-            ingredientsText.append("\t\t&#8226;");
+            ingredientsText.append("\t\t•");
             ingredientsText.append(ingredient.getName());
-            ingredientsText.append(" - ");
+            ingredientsText.append(", ");
             ingredientsText.append(ingredient.getAmount());
-            ingredientsText.append("<br/>");
+            ingredientsText.append("\n");
         }
         recipeIngredients.setText(ingredientsText);
         instructionsText.setText(recipe.getInstructions());
         new SetImageToURLTask(recipeDetailImage).execute(recipe.getThumbnailURL().toString());
+
+        //Create button events
+        backButton.setOnClickListener(view -> {
+            onBackPressed();
+            finish();
+        });
     }
 
+    public void updateFavoriteButton() {
+        if(!favoritesService.isFavorite(recipe.getId())) {
+            favoriteButton.setText(getString(R.string.favorite_button));
+            favoriteButton.setOnClickListener(view -> {
+                favoritesService.addFavorite(recipe);
+                updateFavoriteButton();
+            });
+        }else {
+            favoriteButton.setText(getString(R.string.unfavorite_button));
+            favoriteButton.setOnClickListener(view -> {
+                favoritesService.removeFavorite(recipe.getId());
+                updateFavoriteButton();
+            });
+        }
+    }
 
+    public void initFavoritesService() {
+        Intent bindFavoritesServiceIntent = new Intent(this, FavoritesService.class);
+        bindService(bindFavoritesServiceIntent, favoritesServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private final ServiceConnection favoritesServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            //Get the instance of the BMIService
+            FavoritesService.FavoritesBinder binder = (FavoritesService.FavoritesBinder) service;
+            //References to the BMI Service
+            favoritesService = binder.getService();
+            updateFavoriteButton();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
 }
