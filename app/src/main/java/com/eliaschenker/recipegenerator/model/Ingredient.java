d6b7455eb860125dbf8cc975660d15e8df93cf5a
EliaSchenker <78470999+eliaSchenker@users.noreply.github.com>
2022-07-07 16:36:22 +0200

    Model classes
    Added Recipe and Ingredient model classes

---
+++ app/src/main/java/com/eliaschenker/recipegenerator/model/Ingredient.java
@@ -0,0 +1,33 @@
+package com.eliaschenker.recipegenerator.model;
+
+import java.io.Serializable;
+
+public class Ingredient implements Serializable {
+    private String name;
+    private String amount;
+
+    public Ingredient() {
+
+    }
+
+    public Ingredient(String name, String amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
--- app/src/main/java/com/eliaschenker/recipegenerator/model/Recipe.java
+++ app/src/main/java/com/eliaschenker/recipegenerator/model/Recipe.java
@@ -1,5 +1,97 @@
 package com.eliaschenker.recipegenerator.model;
 
-public class Recipe {
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
 
+public class Recipe implements Serializable {
+    private String name;
+    private String category;
+    private String area;
+    private String instructions;
+    private Ingredient[] ingredients;
+    private URL thumbnailURL;
+    private URL sourceURL;
+
+    public Recipe() {
+
+    }
+
+    public Recipe(String name, String category, String area, String instructions, Ingredient[] ingredients, URL thumbnailURL, URL sourceURL) {
+        this.name = name;
+        this.category = category;
+        this.area = area;
+        this.ingredients = ingredients;
+        this.thumbnailURL = thumbnailURL;
+        this.sourceURL = sourceURL;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public URL getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        try {
+            if (!Objects.equals(thumbnailURL, "")) {
+                this.thumbnailURL = new URL(thumbnailURL);
+            }
+        }catch(MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public URL getSourceURL() {
+        return sourceURL;
+    }
+
+    public void setSourceURL(String sourceURL) {
+        try {
+            if (!Objects.equals(sourceURL, "")) {
+                this.sourceURL = new URL(sourceURL);
+            }
+        }catch(MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 }
